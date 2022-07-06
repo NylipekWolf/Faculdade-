88b6fab12a36a6b0e54ed4d1700c6eb10171b4de
Kepilyn V <80963070+NylipekWolf@users.noreply.github.com>
2022-07-05 22:20:00 -0300

    main

---
+++ index.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+class Objeto:
+    def __init__(self, tipo, data, valor):
+        self.tipo = tipo
+        self.data = data
+        self.valor = valor
+        pass
+
+    def criacao():
+        while True:
+            tipo = input('Seria uma 1.Despesa ou 2.Receita :')
+            if tipo.isnumeric():
+                if(tipo.isnumeric() == 1):
+                    tipo = 'Despesa'
+                elif(tipo.isnumeric() == 2):
+                    tipo = 'Receita'
+                break
+            print('Digitou errado')
+        while True:
+            data = input('Digite a data da compra :')
+            break
+
+while True:
+    print('Bem vindo ao sistema')
+    Objeto.criacao()
+
+        
